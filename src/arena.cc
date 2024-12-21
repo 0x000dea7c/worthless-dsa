596dd70b43ce58ca3f268793b17deacc4fca3868
whocares <flevn@protonmail.com>
2024-12-21 17:49:35 +0100

    Basic arena

--- src/arena.cc
+++ src/arena.cc
@@ -11,12 +11,25 @@
 #include <cstdio>
 
 using U0 = void;
-using U8 = unsigned char;
+using U8 = std::uint8_t;
 using I32 = std::int32_t;
 using U32 = std::uint32_t;
 using U64 = std::uint64_t;
+using F32 = float;
+
+constexpr F32 PI = 3.14159265;
 
 #define internal static
+
+inline internal U0
+prove (bool expr, char const* msg)
+{
+  if (!expr)
+    {
+      std::fprintf(stderr, "%s\n", msg);
+      *(int *) 0 = 0; // blow the fuck up!
+    }
+}
 
 inline internal constexpr U32
 kilobytes (U32 n)
@@ -36,19 +49,43 @@ gigabytes (U32 n)
   return megabytes (n) * 1024ull;
 }
 
+inline internal constexpr bool
+is_power_of_2 (U32 n)
+{
+  return (n & (n - 1)) == 0;
+}
+
 struct arena
 {
+  arena (U8 *buff, U64 l, U64 o) : buffer{buff}, len{l}, offset{o} {}
   U8 *buffer;
   U64 len;
   U64 offset;
 };
 
-[[nodiscard]] internal U8 *
-arena_alloc (arena &a, std::size_t size)
+[[nodiscard]] internal std::uintptr_t
+align_forward (std::uintptr_t ptr, std::size_t alignment)
 {
-  if (a.offset + size <= a.len)
+  prove (is_power_of_2 (alignment), "My alignment isn't a power of 2");
+  std::uintptr_t p, a, mod;
+  p = ptr;
+  a = alignment;
+  mod = p & (a - 1); // this is the same as p % a, but faster bc % is very expensive
+  if (mod != 0)      // if a isn't a factor of p then it's not aligned, so align it
+    p += a - mod;    // it's nothing special, just modulo arithmetic, try it with some numbers if I don't remember
+  return p;
+}
+
+[[nodiscard]] U8 *
+arena_alloc (arena &a, std::size_t size, std::size_t alignment = 2 * sizeof (U0 *)) // in this machine: 16 bytes
+{
+  // I want to align bc computers want to access memory locations in powers of 2 since the size of a word is a
+  // power of 2. If I don't align memory then what could have been a single read could degrade to two reads.
+  std::uintptr_t p = (std::uintptr_t) a.buffer + (std::uintptr_t) a.offset;
+  std::uintptr_t rel_offset = align_forward (p, alignment) - (std::uintptr_t) a.buffer;
+  if (rel_offset + size <= a.len)
     {
-      // TODO: NOT ALIGNED: problem!
+      a.offset = rel_offset;
       auto *ptr = &a.buffer[a.offset];
       a.offset += size;
       std::memset (ptr, 0, size);
@@ -57,29 +94,50 @@ arena_alloc (arena &a, std::size_t size)
   return nullptr;
 }
 
+U0
+arena_free (arena &a)
+{
+  a.offset = 0;
+}
+
+U0
+store_str (char* dst, char const *src, std::size_t chunk_size)
+{
+  auto len = std::strlen(src) + 1;
+  prove (chunk_size >= len, "I don't have enough space to store your str");
+  std::strncpy(dst, src, len);
+}
+
 int
 main ()
 {
-  arena my_arena;
-  my_arena.len = megabytes (2);
-  my_arena.offset = 0;
-  my_arena.buffer = (U8 *) mmap (nullptr, my_arena.len, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-  if (!my_arena.buffer)
+  auto size = megabytes (2);
+  auto *buff = (U8 *) mmap (nullptr, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (!buff)
     {
       perror ("couldn't allocate any memory");
       return 1;
     }
 
-  // TODO: this is also very hacky!
-  I32 *buff = (I32 *) arena_alloc (my_arena, megabytes (1));
+  arena my_arena{buff, size, 0};
+  char *str = (char *) arena_alloc (my_arena, kilobytes (32));
+  std::strncpy (str, "Hello World", 12);
+  __builtin_printf ("Your string is: %s\n", str);
+  arena_free (my_arena);
 
-  for (U32 i = 0; i < 32; ++i)
-    buff[i] = i + 1;
+  auto chunk_size = 23;
+  str = (char *) arena_alloc (my_arena, chunk_size);
+  store_str (str, "Is this going to work?", chunk_size);
+  __builtin_printf ("Your new string is: %s\n", str);
 
-  for (U32 i = 0; i < 32; ++i)
-    __builtin_printf ("%d ", buff[i]);
-
-  __builtin_printf ("\n");
+  auto const n = 32;
+  auto const array_size = 4 * n;     // store 32 elements
+  F32 *array = (F32 *) arena_alloc(my_arena, array_size);
+  prove(array, "oops, couldn't allocate space for my array");
+  for (U32 i = 0; i < n; ++i)
+    array[i] = PI + i * 2;
+  for (U32 i = 0; i < n; ++i)
+    __builtin_printf ("array[%u] = %f\n", i, array[i]);
 
   if (munmap (my_arena.buffer, my_arena.len) == -1)
     perror ("couldn't deallocate memory");
